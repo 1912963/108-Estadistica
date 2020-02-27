5786cbd8b4a62f0c5ccb9b1e33456cbf7b2061ec
Adriana Garza <adriana.c_gza@hotmail.com>
2020-02-27 09:25:52 -0600

    cedro

---
+++ Clase/cedro rojo clase.R
@@ -0,0 +1,13 @@
+#Adriana Concepción Garza Pérez
+#1912963
+#27/02/20
+
+
+
+# importar base de datos de cedro rojo ------------------------------------
+
+cr <- read.csv("Clase/cedro-rojo.scv", header = T)
+t.test(cr$diametro, nu=12.7)
+t.test((cr$altura, nu=19)
+
+       
